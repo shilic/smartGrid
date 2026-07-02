8d61004d8e2700e4cfd0b78e8798493c33f5b32c
石李城 <985478238@qq.com>
2026-07-02 19:11:00 +0800

    更新 MainTest.kt

--- src/test/kotlin/demo/MainTest.kt
+++ src/test/kotlin/demo/MainTest.kt
@@ -22,7 +22,7 @@ class MainTest {
         // 路径使用相对路径
         val filePath = "src/test/resources/excel/DBC模版.xlsx"
         // 使用路径实例化一个 workbook 并解析出数据。如果你想使用其他表格组件，使用适配器模式，让新组件实现 Workbook 系列接口即可实现任意表格的解析，不局限于EXCEL表格。
-        val canProtocols = GridReader(filePath.workbook()).read(CanProtocol::class)
+        val canProtocols: MutableMap<String, CanProtocol> = GridReader(filePath.workbook()).read(CanProtocol::class)
         println("canProtocols: ${canProtocols.toGsonString()}")
     }
 }
